e52a0ea310513e1756fc6462a66ea6830ac03214
Andre Menezes <djdedemenezes@gmail.com>
2024-11-21 19:50:28 -0300

    refac resquest response into new component and add some styling

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,30 @@
 <script setup>
 import NavBar from './components/NavBar.vue'
 import HomeBanner from './components/HomeBanner.vue'
-import RequestForm from './components/RequestForm.vue'
+import ApiRequest from './components/ApiRequest.vue'
 </script>
 
 <template>
   <NavBar />
   <HomeBanner />
-  <div class="container">
-    <RequestForm />
-    <pre class="response tree" id="response" data-request-target="pre"></pre>
-  </div>
+  <ApiRequest />
 </template>
 
-<style scoped></style>
+<style>
+.response {
+  color: #3a3f44;
+  background-color: #f5f5f5;
+  border-radius: 4px;
+  tab-size: 4;
+  word-break: break-all;
+  word-wrap: break-word;
+  display: block;
+  padding: 9.5px;
+  margin: 0 0 10px;
+  font-size: 0.825rem;
+  max-height: 80vh;
+}
+.tree .json-container {
+  background-color: #f5f5f5 !important;
+}
+</style>
---
+++ src/components/ApiRequest.vue
@@ -0,0 +1,9 @@
+<script setup>
+import RequestForm from './RequestForm.vue'
+</script>
+<template>
+  <div class="container">
+    <RequestForm />
+    <pre class="response tree" id="response" data-request-target="pre"></pre>
+  </div>
+</template>
